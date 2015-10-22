36e9f6ae7edf84e3983a9aa4a0f4d7cf3cad9045
zh <zhy_ho@126.com>
2015-10-22 19:40:21 +0800

    android readme

--- Readme.mk
+++ Readme.mk
@@ -1,6 +1,19 @@
 ###Read me
-Android Sdk 20+
-Android 权限 SDCard RW
+tools of android developer
 ------------------------
 2015 10-22 19:20 released version 1.0.2
 modify sdk version
+
+------------------------
+Android Sdk 8+
+Android permission
+
+<uses-permission android:name="android.permission.INTERNET" />
+<uses-permission android:name="android.permission.READ_PHONE_STATE" />
+<uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+------------------------
+last version 1.0.2
+use in android studio
+in build.gradle [compile 'cn.isif.alibs:alibs:1.0.2']
